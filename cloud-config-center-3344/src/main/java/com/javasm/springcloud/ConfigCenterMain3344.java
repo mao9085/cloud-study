fb28f2e22f6c1ab92e78ed322d5eda594d25c1a1
wang <www@qq.com>
2021-08-24 00:10:43 +0800

    sss

--- cloud-config-center-3344/src/main/java/com/javasm/springcloud/ConfigCenterMain3344.java
+++ cloud-config-center-3344/src/main/java/com/javasm/springcloud/ConfigCenterMain3344.java
@@ -3,8 +3,10 @@ package com.javasm.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableConfigServer
 public class ConfigCenterMain3344 {
     public static void main(String[] args) {
